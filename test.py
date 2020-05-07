f7516648f60fb3a5b2b2c3f7564f5d2c0aee1b1d
Anders Bredgaard Thuesen <andersbthuesen@gmail.com>
2020-05-07 09:29:52 +0200

    Stuff

--- test.py
+++ test.py
@@ -29,7 +29,7 @@ class BayesianNN(PyroModule):
   def __init__(self, input_size, hidden_size, output_size):
     super(BayesianNN, self).__init__()
     self.fc1 = PyroModule[nn.Linear](input_size, hidden_size)
-    self.fc1.weight = PyroSample(dist.Normal(0., 2.).expand([hidden_size, input_size]).to_event(2))
+    self.fc1.weight = PyroSample(dist.Normal(0., 1.).expand([hidden_size, input_size]).to_event(2))
     self.fc1.bias = PyroSample(dist.Normal(0., 10.).expand([hidden_size]).to_event(1))
 
     self.out = PyroModule[nn.Linear](hidden_size, output_size)
@@ -47,6 +47,41 @@ class BayesianNN(PyroModule):
       obs = pyro.sample("obs", dist.Normal(mean, sigma), obs=y)
 
     return mean
+
+
+def model(X, y=None):
+  hidden_size = 10
+  input_size = 1
+  output_size = 1
+
+  w1_mean = torch.zeros((hidden_size, input_size))
+  w1_std = torch.ones((hidden_size))
+
+  w2_mean = torch.zeros((output_size, hidden_size))
+  w2_std = torch.ones((output_size))
+
+  a1 = pyro.sample("a1", F.relu(X @ dist.Normal(w1_mean, w1_std)))
+  y = pyro.sample("y", a1 @ dist.Normal(w2_mean, w2_std), dist=y)
+
+  
+def guide(X, y):
+  hidden_size = 10
+  input_size = 1
+  output_size = 1
+
+  w1_mean = pyro.param("w1.mean", dist.Normal(0., 1.).expand([input_size, hidden_size]))
+  w1_std = pyro.param("w1.std", dist.Normal(0., 5.).expand([1, hidden_size]))
+
+  w2_mean = pyro.param("w2.mean", dist.Normal(0., 1.).expand([output_size, hidden_size]))
+  w2_std = pyro.param("w2.std", dist.Normal(0., 5.).expand([output_size]))
+
+  w1 = pyro.sample("w1", dist.Normal(w1_mean, w1_std))
+  w2 = pyro.sample("w2", dist.Normal(w2_mean, w2_std))
+
+  with pyro.iarange("data", X.size(0)) as ids:
+    a1 = pyro.sample("a1", F.relu(X[ids] @ w1))
+    y = pyro.sample("y", w2 @ a1)
+
 
 def train(model, guide, dataloader, num_epochs=50):
   optim = Adam({ "lr": 0.01 })
@@ -69,12 +104,10 @@ if __name__ == "__main__":
   dataset = SimpleDataset(X, y)
   dataloader = DataLoader(dataset, batch_size=4)
 
-  bnn = BayesianNN(1, 1, 1)
-  guide = AutoDiagonalNormal(bnn)
+  #bnn = BayesianNN(1, 1, 1)
+  #guide = AutoDiagonalNormal(bnn)
 
-  
-
-  train(bnn, guide, dataloader);
+  train(model, guide, dataloader);
 
   plt.scatter(X, y)
   plt.scatter(X, bnn(X.view((X.shape[0], -1))))
